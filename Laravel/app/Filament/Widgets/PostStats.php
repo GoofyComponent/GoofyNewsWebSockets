4ae2026a6674329e9351938331616ca578b66724
Adrien Albuquerque <adrialb95@gmail.com>
2024-12-18 15:23:12 +0100

    WEBSOCKET

--- Laravel/app/Filament/Widgets/PostStats.php
+++ Laravel/app/Filament/Widgets/PostStats.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Widgets;
 
 use App\Models\Post;
+use Filament\Widgets\ChartWidget;
 use Illuminate\Support\Facades\Log;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
@@ -25,7 +26,6 @@ class PostStats extends BaseWidget
 
     public function refresh()
     {
-        Log::info('Refreshing post stats widget...');
         $this->stats = [
             'totalPosts' => Post::count(),
             'publishedPosts' => Post::where('is_published', true)->count(),
